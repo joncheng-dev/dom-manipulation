0039a8c5598517b5fc49f8b2c930e31fbc0023b6
Jonathan Cheng <jcheng461@gmail.com>
2022-02-25 00:17:20 +0900

    add ability to remove prepended messages on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,50 @@
 $(document).ready(function () {
   $("button#hello").click(function () {
-    $("ul").prepend("<li>Hello!</li>");
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Hello to you too!</li>");
+    $("ul#user")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
+    $("ul#webpage")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
   });
   $("button#goodbye").click(function () {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>See you later!</li>");
+    $("ul#user")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
+    $("ul#webpage")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
   });
   $("button#stop").click(function () {
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Okay fine.</li>");
+    $("ul#user")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
+    $("ul#webpage")
+      .children("li")
+      .first()
+      .click(function () {
+        $(this).remove();
+      });
   });
 });
